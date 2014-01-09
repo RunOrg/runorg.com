12287a15ddcb720a9f507fde4bc29aa228615d64
Victor Nicollet <victor@nicollet.net>
2014-01-09 14:00:26 +0100

    Jekyll run batch

---
+++ run-jekyll.bat
@@ -0,0 +1,2 @@
+set PATH=%PATH%;C:\Program Files\PortableJekyll\ruby\bin;C:\Program Files\PortableJekyll\devkit\bin;C:\Program Files\PortableJekyll\git\bin;C:\Program Files\PortableJekyll\Python\App;C:\Program Files\PortableJekyll\devkit\mingw\bin
+jekyll serve
