a937e00dd2dbe099d193c0399e88ec8d8c8f11b8
Victor Nicollet <victor@nicollet.net>
2014-01-10 22:12:03 +0100

    Fix UTF-8

--- run-jekyll.bat
+++ run-jekyll.bat
@@ -1,2 +1,3 @@
 set PATH=%PATH%;C:\Program Files\PortableJekyll\ruby\bin;C:\Program Files\PortableJekyll\devkit\bin;C:\Program Files\PortableJekyll\git\bin;C:\Program Files\PortableJekyll\Python\App;C:\Program Files\PortableJekyll\devkit\mingw\bin
+set LANG=en_EN.UTF-8
 jekyll serve
